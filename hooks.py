25df772b82671cdf34ade0653c5cf510d1f839d9
Albin Parou <fumesover@gmail.com>
2019-04-18 21:21:03 +0900

    Feat (hooks): Add support for specific config updates

--- hooks.py
+++ hooks.py
@@ -137,9 +137,93 @@ async def checkupdates(client, config):
         confirmed.append(data['id'])
 
     async def updateconfig(data):
-        guild_id = data['server']
-        await api.update_conf(client, config, guild_id)
-        confirmed.append(data['id'])
+        value = data['value'].split('-')
+
+        print(data)
+        async def channels_update(data):
+            await api.update_conf(client, config, data['server'])
+            print(data['author'], 'updated the configuration of', data['server'], ', new config is: ...')
+        async def deldomain(data):
+            config['servers'][data['server']]['domains'].remove(data['email'])
+            print(data['author'], 'removed a domain in the whitelist of', data['server'])
+            print('whitelisted domains :', config['servers'][data['server']]['domains'])
+        async def adddomain(data):
+            config['servers'][data['server']]['domains'].append(data['ban_type'])
+            print(data['author'], 'added a domain in the whitelist of', data['server'])
+            print('whitelisted domains :', config['servers'][data['server']]['domains'])
+        async def activate(data):
+            config['servers'][data['server']]['is_active'] = True
+            print(data['author'], 'activated the server', data['server'])
+        async def deactivate(data):
+            config['servers'][data['server']]['is_active'] = False
+            print(data['author'], 'deactivated the server', data['server'])
+        async def delrank(data):
+            async def delconfirmed():
+                config['servers'][data['server']]['ranks']['confirmed'].remove(int(data['email']))
+                print(data['author'], 'removed confirmed rank', data['email'])
+            async def delbanned():
+                config['servers'][data['server']]['ranks']['banned'].remove(int(data['email']))
+                print(data['author'], 'removed banned rank', data['email'])
+            async def delclassic():
+                config['servers'][data['server']]['ranks']['classic'][data['ban_type']].remove(int(data['email']))
+                print(data['author'], 'removed classic rank', data['email'])
+
+            handelers = {
+                'confirmed': delconfirmed,
+                'banned': delbanned,
+                'classic': delclassic,
+            }
+
+            if value[1] in handelers:
+                print('Old config:', config['servers'][data['server']])
+                await handelers[value[1]]()
+                confirmed.append(data['id'])
+                print('New config:', config['servers'][data['server']])
+            else:
+                print('wtf ?', data)
+        async def addrank(data):
+            async def addconfirmed():
+                config['servers'][data['server']]['ranks']['confirmed'].append(int(data['email']))
+                print(data['author'], 'added confirmed rank', data['email'])
+            async def addbanned():
+                config['servers'][data['server']]['ranks']['banned'].append(int(data['email']))
+                print(data['author'], 'added banned rank', data['email'])
+            async def addclassic():
+                if not data['ban_type'] in config['servers'][data['server']]['ranks']['classic']:
+                    config['servers'][data['server']]['ranks']['classic'][data['ban_type']] = []
+                config['servers'][data['server']]['ranks']['classic'][data['ban_type']].append(int(data['email']))
+                print(data['author'], 'added classic rank', data['email'])
+
+            handelers = {
+                'confirmed': addconfirmed,
+                'banned': addbanned,
+                'classic': addclassic,
+            }
+
+            if value[1] in handelers:
+                print('Old config:', config['servers'][data['server']])
+                await handelers[value[1]]()
+                confirmed.append(data['id'])
+                print('New config:', config['servers'][data['server']])
+            else:
+                print('wtf ?', data)
+
+        handelers = {
+            'channels': channels_update,
+            'deldomain': deldomain,
+            'adddomain': adddomain,
+            'activate': activate,
+            'deactivate': deactivate,
+            'addrank': addrank,
+            'delrank': delrank
+        }
+
+        if value[0] in handelers:
+            print(value[0])
+            await handelers[value[0]](data)
+            confirmed.append(data['id'])
+        else:
+            print('Unknown : ', data)
 
     handelers = {
         'certify': certify,
@@ -151,6 +235,7 @@ async def checkupdates(client, config):
     }
 
     for update in data:
+        print(update)
         if 'type' in update and update['type'] in handelers:
             await handelers[update['type']](update)
 
